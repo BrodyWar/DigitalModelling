8b19c604550c3fd69472eade2d647299a5312c98
Nikolai <71685687+BrodyWar@users.noreply.github.com>
2022-01-24 19:02:14 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 #include <vector>
 #include <cmath>
 
+double g = 9.81, k = 1, r = 0.6, len = 15;
 
-int p = 1;
+bool f1 = false, f2 = false;
 
 struct State {
     double x, y;
@@ -286,67 +287,36 @@ public:
 
 
 void f(BodyA* body, BodyA* dot) {
-    double g = 9.81, k = 1, r = 0.6, len = 20;
-    double j = 150;
     double m = body[0].m;
-    double impuls = (1.0 / 12.0) * (std::pow(body[0]._shape->a, 2) + std::pow(body[0]._shape->b, 2)) * m;
+    double moment = (1.0 / 12.0) * (std::pow(body[0]._shape->a, 2) + std::pow(body[0]._shape->b, 2)) * m;
 
     Shape el = body[0].global(body[0]._shape);
     Wheels wheels = body[0].global(body[0]._wheels);
     double a = body[0].a / 180.0 * M_PI; // translate grad to rad
-    dot[0].w = 0;
-
-
-    if ((el.y[0] <= 0 || el.y[1] <= 0 || el.y[2] <= 0 || el.y[3] <= 0) && body[0].mov.y < 0) {
-        dot[0].pos.y = 0;
-        body[0].mov.y = 0;
-        if (el.y[2] <= 0)
-            dot[0].w += -j *  std::fabs(std::cos(a)) * (el.x[2] - body[0].pos.x) / (impuls);
-        if (el.y[3] <= 0)
-            dot[0].w += j  * std::fabs(std::cos(a)) * (el.x[3] - body[0].pos.x) / (impuls);
-        if (el.y[1] <= 0)
-            dot[0].w += -j  * std::fabs(std::cos(a)) * (el.x[1] - body[0].pos.x) / (impuls);
-        if (el.y[0] <= 0)
-            dot[0].w += j  * std::fabs(std::cos(a)) * (el.x[0] - body[0].pos.x) / (impuls);
-    }
-    
-
 
 
     
-    if (wheels.pos[0].y <= 0) { // left wheel
-        body[0]._wheels->mov[0].y = -body[0].mov.y -std::sqrt(std::pow(body[0]._wheels->pos[0].y - body[0].pos.y, 2) + std::pow(body[0]._wheels->pos[0].x - body[0].pos.x, 2)) * body[0].w / impuls;
-        dot[0].w += k * (body[0]._shape->y[0] - body[0]._wheels->pos[0].y) * std::fabs(std::cos(a)) * (el.x[0] - body[0].pos.x) / (impuls);
-    }
-    if (wheels.pos[1].y <= 0) { // right wheel
-        body[0]._wheels->mov[1].y = -body[0].mov.y -std::sqrt(std::pow(body[0]._wheels->pos[1].y - body[0].pos.y, 2) + std::pow(body[0]._wheels->pos[1].x - body[0].pos.x, 2)) * body[0].w / impuls;
-        dot[0].w += k * (body[0]._shape->y[3] - body[0]._wheels->pos[1].y) * std::fabs(std::cos(a)) * (el.x[3] - body[0].pos.x) / (impuls);
-    }
-    //std::sqrt(std::pow(wheels.pos[0].y - el.y[0],2) + std::pow(wheels.pos[0].x-el.x[0],2))
+
 
     dot[0].a = body[0].w * 180 / M_PI; // translate rad to grad
-    
-
-
 
     dot[0].pos.x = body[0].mov.x;
     dot[0].pos.y = body[0].mov.y;
     dot[0].mov.x = 0;
 
-    dot[0].mov.y = -g - k * (body[0]._shape->y[0] - body[0]._wheels->pos[0].y - len) * std::fabs(std::cos(a)) - double(r*body[0].mov.y)
-        - k * (body[0]._shape->y[3] - body[0]._wheels->pos[1].y - len) * std::fabs(std::cos(a)) - double(r*body[0].mov.y);
+    dot[0].mov.y =- g -k / m * (body[0]._shape->y[0] - body[0]._wheels->pos[0].y - len) - double(r * body[0].mov.y)
+        - k / m * (body[0]._shape->y[3] - body[0]._wheels->pos[1].y - len) -double(r * body[0].mov.y);
 
     dot[0]._wheels->pos[0].x = 0;
     dot[0]._wheels->pos[0].y = body[0]._wheels->mov[0].y;
-    dot[0]._wheels->mov[0].x = body[0]._wheels->mov[0].x;
-    dot[0]._wheels->mov[0].y = -g + k / m * (body[0]._shape->y[0] - body[0]._wheels->pos[0].y - len) * std::fabs(std::cos(a)) + double(r *body[0].mov.y);
+    dot[0]._wheels->mov[0].x = 0;
+    dot[0]._wheels->mov[0].y = - g + k / m * (body[0]._shape->y[0] - body[0]._wheels->pos[0].y - len) +double(r * body[0].mov.y);
 
 
     dot[0]._wheels->pos[1].x = 0;
     dot[0]._wheels->pos[1].y = body[0]._wheels->mov[1].y;
-    dot[0]._wheels->mov[1].x = body[0]._wheels->pos[1].x;
-    dot[0]._wheels->mov[1].y = -g + k / m * (body[0]._shape->y[3] - body[0]._wheels->pos[1].y - len) * std::fabs(std::cos(a)) + double(r*body[0].mov.y);
-
+    dot[0]._wheels->mov[1].x = 0;
+    dot[0]._wheels->mov[1].y = - g + k / m * (body[0]._shape->y[3] - body[0]._wheels->pos[1].y - len) +double(r * body[0].mov.y);
 
 
 }
@@ -389,58 +359,109 @@ int main()
     }
 
     BodyA body[1];
-    body[0] = BodyA(State(0, 50), 0); // center (x,y), angle[grad]
-    double h = 0.02;
+    body[0] = BodyA(State(0, 50), 40); // center (x,y), angle[grad]
+    double h = 0.015;
 
     glfwMakeContextCurrent(window);
     glScaled(0.01f, 0.01f, 0.0);
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glClearColor(0.2f, 0.3f, 0.6f, 0.0f);
+        {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glClearColor(0.2f, 0.3f, 0.6f, 0.0f);
 
+
+            Shape el = body[0].global(body[0]._shape);
+            Wheels wheels = body[0].global(body[0]._wheels);
+
+            glLineWidth(3);
+            glBegin(GL_QUADS);
+            glColor3f(0.0f, 0.0f, 0.0f);
+
+            glVertex2f(el.x[0], el.y[0]);
+            glVertex2f(el.x[1], el.y[1]);
+            glVertex2f(el.x[2], el.y[2]);
+            glVertex2f(el.x[3], el.y[3]);
+            glEnd();
+
+            glLineWidth(1);
+            glBegin(GL_LINES);
+            glColor3f(1.0f, 1.0f, 1.0f);
+            glVertex2f(-70, 0);
+            glVertex2f(70, 0);
+            glColor3f(0.4f, 1.0f, 0.7f);
+            glVertex2f(wheels.pos[0].x, wheels.pos[0].y);
+            glVertex2f(el.x[0], el.y[0]);
+
+            glVertex2f(wheels.pos[1].x, wheels.pos[1].y);
+            glVertex2f(el.x[3], el.y[3]);
+
+            glEnd();
+
+
+            glPointSize(4);
+            glBegin(GL_POINTS);
+            glColor3f(0.0f, 0.0f, 0.0f);
+            glVertex2f(wheels.pos[0].x, wheels.pos[0].y);
+            glVertex2f(wheels.pos[1].x, wheels.pos[1].y);
+            glEnd();
+
+
+            glfwSwapBuffers(window);
+            glFlush();
+            glfwPollEvents();
+        }
+
+
+        double m = body[0].m;
+        double a = body[0].a / 180.0 * M_PI; // translate grad to rad
+        double moment = (1.0 / 12.0) * (std::pow(body[0]._shape->a, 2) + std::pow(body[0]._shape->b, 2)) * m;
+        Shape el1 = body[0].global(body[0]._shape);
+        Wheels wheels1 = body[0].global(body[0]._wheels);
+
+        double tmp = body[0].w;
+        if ((el1.y[0] <= 0 || el1.y[1] <= 0 || el1.y[2] <= 0 || el1.y[3] <= 0) && body[0].mov.y < 0) {
+            body[0].mov.y = 0;  
+            if (el1.y[2] <= 0)
+                body[0].w += -g * (el1.x[2] - body[0].pos.x) / (moment);
+            if (el1.y[3] <= 0)
+                body[0].w += g * (el1.x[3] - body[0].pos.x) / (moment);
+            if (el1.y[1] <= 0)
+                body[0].w += -g * (el1.x[1] - body[0].pos.x) / (moment);
+            if (el1.y[0] <= 0)
+                body[0].w += g * (el1.x[0] - body[0].pos.x) / (moment);
+            if (std::fabs(body[0].w - tmp) < 0.1) {
+                body[0].w = 0;
+            }
+        }
+        
+        double curlen1 = body[0]._shape->y[0] - body[0]._wheels->pos[0].y, curlen2 = body[0]._shape->y[3] - body[0]._wheels->pos[1].y;
+
+        if (wheels1.pos[0].y <= 0) { // left wheel
+
+
+            body[0]._wheels->mov[0].y = -body[0].mov.y - wheels1.pos[0].y;
+            body[0].w += k * std::fabs(std::sqrt(std::pow(wheels1.pos[0].y - el1.y[0], 2) + std::pow(wheels1.pos[0].x - el1.x[0], 2)) - len)  * (el1.x[0] - body[0].pos.x) / (moment);
+
+            if (std::fabs(body[0]._wheels->mov[0].y) < 0.25)
+                body[0]._wheels->mov[0].y = 0;
+
+
+        }
+
+        if (wheels1.pos[1].y <= 0) { // right wheel
+            if (std::fabs(body[0]._wheels->mov[1].y) < 0.25)
+                body[0]._wheels->mov[1].y = 0;
+
+            body[0]._wheels->mov[1].y = -body[0].mov.y - wheels1.pos[1].y;
+            body[0].w += k * std::fabs(std::sqrt(std::pow(wheels1.pos[1].y - el1.y[3], 2) + std::pow(wheels1.pos[1].x - el1.x[3], 2)) - len)  * (el1.x[3] - body[0].pos.x) / (moment);
+
+
+
+        }
+        body[0].w = body[0].w * 0.8;
 
         RungeKutta(body, h, f);
-
-        Shape el = body[0].global(body[0]._shape);
-        Wheels wheels = body[0].global(body[0]._wheels);
-
-        glLineWidth(3);
-        glBegin(GL_QUADS);
-        glColor3f(0.0f, 0.0f, 0.0f);
-
-        glVertex2f(el.x[0], el.y[0]);
-        glVertex2f(el.x[1], el.y[1]);
-        glVertex2f(el.x[2], el.y[2]);
-        glVertex2f(el.x[3], el.y[3]);
-        glEnd();
-
-        glLineWidth(1);
-        glBegin(GL_LINES);
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex2f(-70, 0);
-        glVertex2f(70, 0);
-        glColor3f(0.4f, 1.0f, 0.7f);
-        glVertex2f(wheels.pos[0].x, wheels.pos[0].y);
-        glVertex2f(el.x[0], el.y[0]);
-
-        glVertex2f(wheels.pos[1].x, wheels.pos[1].y);
-        glVertex2f(el.x[3], el.y[3]);
-
-        glEnd();
-
-
-        glPointSize(4);
-        glBegin(GL_POINTS);
-        glColor3f(0.0f, 0.0f, 0.0f);
-        glVertex2f(wheels.pos[0].x, wheels.pos[0].y);
-        glVertex2f(wheels.pos[1].x, wheels.pos[1].y);
-        glEnd();
-
-
-        glfwSwapBuffers(window);
-        glFlush();
-        glfwPollEvents();
     }
     glfwTerminate();
     return 0;
